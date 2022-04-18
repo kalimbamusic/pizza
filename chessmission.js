aee325b2517a8d10212219b49dae82ce046b3e99
kalimbamusic <102989036+kalimbamusic@users.noreply.github.com>
2022-04-18 22:17:17 +0300

    change

--- chessmission.js
+++ chessmission.js
@@ -1,9 +1,9 @@
-let center = document.createElement('center');
 let ChessTable = document.createElement('table');
 for (i = 0; i < 8; i++) {
     let tr = document.createElement('tr');
     for (j = 0; j < 8; j++) {
         let td = document.createElement('td');
+        td.id = "cell-" + i.toString() + "_" + j.toString();
         if ((i + j) % 2 == 0) {
             td.setAttribute('class', 'cell whitecell');
             tr.appendChild(td);
@@ -13,9 +13,7 @@ for (i = 0; i < 8; i++) {
             tr.appendChild(td);
         }
     }
-    ChessTable.appendChild(tr);
+    // tr.addEventListener('click',td.setAttribute('class','chosencell'))
+    // ChessTable.appendChild(tr);
 }
-center.appendChild(ChessTable);
-ChessTable.setAttribute('cellspacing', '0');
-ChessTable.setAttribute('width', '270px');
-document.body.appendChild(center);
+document.body.appendChild(ChessTable);
