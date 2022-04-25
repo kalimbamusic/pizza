abeb129d9e8819de02eecce332943935c243acdf
kalimbamusic <102989036+kalimbamusic@users.noreply.github.com>
2022-04-25 11:59:16 +0300

    fixed some bugs regarding curly brackets

--- chessmission.js
+++ chessmission.js
@@ -1,106 +1,114 @@
-function possiblemoves(piecetype,position){
-    i = document.querySelector(".maybecell")
-    if ((i !== null )) {
-        (i.classList.remove('maybecell'))
-    }
-    positionrow = position[0]
-    positioncolumn = position[2]
+function possiblemoves(piecetype, position) {
+  document.querySelectorAll(".maybecell").forEach((td) => {
+    td.classList.remove('maybecell')
+    console.log(document.querySelectorAll('.maybecell'))
+  })
+  
+  positionrow = position[0];
+  positioncolumn = position[2];
 
-
-    if (piecetype === '<img src="chess/images/dark/pawn.png">') {
-        elementById((positionrow -1),(positioncolumn))
-         
+  if (piecetype === '<img src="chess/images/dark/pawn.png">') {
+    elementById(positionrow - 1, positioncolumn);
+  }
+  if (
+    piecetype === '<img src="chess/images/dark/rook.png">' ||
+    piecetype === '<img src="chess/images/white/rook.png">'
+  ) {
+    for (positionrow = 0; positionrow < 8; positionrow++) {
+      elementById(positionrow, positioncolumn);
     }
-        if (piecetype === '<img src="chess/images/dark/rook.png">' ) {
-            for (positionrow = 0; positionrow < 8; positionrow++) {
-            elementById((positionrow),(positioncolumn))}
-            for (let positioncolumn = 0; positioncolumn < 8; positioncolumn++) {
-                positionrow = position[0]
-                elementById((positionrow),(positioncolumn))
-        }
-
-    }
-   
-        // }
-function elementById(positionrow,positioncolumn) {
-    let maybecell = document.getElementById((String(positionrow)+ ' '+String(positioncolumn)))
-    // if (positionrow == position[0] && positioncolumn == position[2]){
-    //     maybecell.classList.add('chosencell')
-    // }
-    maybecell.classList.add('maybecell')
-}
+    for (let positioncolumn = 0; positioncolumn < 8; positioncolumn++) {
+      positionrow = position[0];
+      elementById(positionrow, positioncolumn);
+    }}
     
-
-
+  if (
+    piecetype === '<img src="chess/images/dark/bishop.png">' ||
+    piecetype === '<img src="chess/images/white/bishop.png">'
+    ) {
+    for (positionrow = 0; positionrow < 8; positionrow++) {
+      elementById(positionrow, positioncolumn)
+      console.log(positionrow, positioncolumn) 
+      }
+      for (let positioncolumn = 0; positioncolumn < 8; positioncolumn++) {
+      elementById(positionrow, positioncolumn);
+      }
+    
+  }
+  function elementById(positionrow, positioncolumn) {
+    let maybecell = document.getElementById(
+      String(positionrow) + " " + String(positioncolumn)
+    );
+    maybecell.classList.add("maybecell");
+    if (positionrow === position[0] && positioncolumn === position[2]){
+        maybecell.classList.add('chosencell')
+    }
+  }
 }
-function onclick(td){
-    i = document.querySelector(".chosencell")
-    if ((i !== null )) {
-        (i.classList.remove('chosencell'))
-    }  
-   td.currentTarget.classList.add("chosencell")
-   let piecetype = td.currentTarget.innerHTML
-   let position = (td.currentTarget.id)
-//    console.log(td.currentTarget.innerHTML)
-//    console.log(td.currentTarget.id)
-   possiblemoves(piecetype,position)
-
+function onclick(td) {
+  i = document.querySelector(".chosencell");
+  if (i !== null) {
+    i.classList.remove("chosencell");
+  }
+  td.currentTarget.classList.add("chosencell");
+  let piecetype = td.currentTarget.innerHTML;
+  let position = td.currentTarget.id;
+  //    console.log(td.currentTarget.innerHTML)
+  //    console.log(td.currentTarget.id)
+  possiblemoves(piecetype, position);
 }
-function putimageonboard(td,color,type){
-    const image = document.createElement('img')
-        image.src = 'chess/images/' + color + '/' + type + '.png';
-        td.appendChild(image);
+function putimageonboard(td, color, type) {
+  const image = document.createElement("img");
+  image.src = "chess/images/" + color + "/" + type + ".png";
+  td.appendChild(image);
 }
 
 function makechessboard() {
-    let ChessTable = document.createElement('table');
-    for (i = 0; i < 8; i++) {
-        let tr = document.createElement('tr');
-        for (j = 0; j < 8; j++) {
-            let td = document.createElement('td');
-            tr.id = i.toString();
-            td.id = i.toString() + " " + j.toString();
-            if ((i + j) % 2 == 0) {
-                td.setAttribute('class', 'cell whitecell');
-                tr.appendChild(td);
-            }
-            else {
-                td.setAttribute('class', 'cell blackcell');
-                tr.appendChild(td);
-                
-            }
-            
-            td.addEventListener('click', onclick);
-            // td.addEventListener('click', ()=> (td.setAttribute('class', 'cell chosencell')))
-        }
-        ChessTable.appendChild(tr);
+  let ChessTable = document.createElement("table");
+  for (i = 0; i < 8; i++) {
+    let tr = document.createElement("tr");
+    for (j = 0; j < 8; j++) {
+      let td = document.createElement("td");
+      tr.id = i.toString();
+      td.id = i.toString() + " " + j.toString();
+      if ((i + j) % 2 == 0) {
+        td.setAttribute("class", "cell whitecell");
+        tr.appendChild(td);
+      } else {
+        td.setAttribute("class", "cell blackcell");
+        tr.appendChild(td);
+      }
 
+      td.addEventListener("click", onclick);
+      // td.addEventListener('click', ()=> (td.setAttribute('class', 'cell chosencell')))
     }
-    document.body.appendChild(ChessTable);
+    ChessTable.appendChild(tr);
+  }
+  document.body.appendChild(ChessTable);
 }
 function intialboard() {
-    putimageonboard((document.getElementById('0 0')), 'white', 'rook');
-    putimageonboard((document.getElementById('0 7')), 'white', 'rook');
-    putimageonboard((document.getElementById('0 1')), 'white', 'bishop');
-    putimageonboard((document.getElementById('0 6')), 'white', 'bishop');
-    putimageonboard((document.getElementById('0 2')), 'white', 'knight');
-    putimageonboard((document.getElementById('0 5')), 'white', 'knight');
-    putimageonboard((document.getElementById('0 3')), 'white', 'king');
-    putimageonboard((document.getElementById('0 4')), 'white', 'queen');
-    putimageonboard((document.getElementById('7 0')), 'dark', 'rook');
-    putimageonboard((document.getElementById('7 7')), 'dark', 'rook');
-    putimageonboard((document.getElementById('7 1')), 'dark', 'bishop');
-    putimageonboard((document.getElementById('7 6')), 'dark', 'bishop');
-    putimageonboard((document.getElementById('7 2')), 'dark', 'knight');
-    putimageonboard((document.getElementById('7 5')), 'dark', 'knight');
-    putimageonboard((document.getElementById('7 4')), 'dark', 'king');
-    putimageonboard((document.getElementById('7 3')), 'dark', 'queen');
-    for (let i = 0; i < 8; i++) {
-        putimageonboard((document.getElementById('6 ' + i)), 'dark', 'pawn');
-    }
-        for (let i = 0; i < 8; i++) {
-        putimageonboard((document.getElementById('1 ' + i)), 'white', 'pawn');    
-    }  
+  putimageonboard(document.getElementById("0 0"), "white", "rook");
+  putimageonboard(document.getElementById("0 7"), "white", "rook");
+  putimageonboard(document.getElementById("0 1"), "white", "bishop");
+  putimageonboard(document.getElementById("0 6"), "white", "bishop");
+  putimageonboard(document.getElementById("0 2"), "white", "knight");
+  putimageonboard(document.getElementById("0 5"), "white", "knight");
+  putimageonboard(document.getElementById("0 3"), "white", "king");
+  putimageonboard(document.getElementById("0 4"), "white", "queen");
+  putimageonboard(document.getElementById("7 0"), "dark", "rook");
+  putimageonboard(document.getElementById("7 7"), "dark", "rook");
+  putimageonboard(document.getElementById("7 1"), "dark", "bishop");
+  putimageonboard(document.getElementById("7 6"), "dark", "bishop");
+  putimageonboard(document.getElementById("7 2"), "dark", "knight");
+  putimageonboard(document.getElementById("7 5"), "dark", "knight");
+  putimageonboard(document.getElementById("7 4"), "dark", "king");
+  putimageonboard(document.getElementById("7 3"), "dark", "queen");
+  for (let i = 0; i < 8; i++) {
+    putimageonboard(document.getElementById("6 " + i), "dark", "pawn");
+  }
+  for (let i = 0; i < 8; i++) {
+    putimageonboard(document.getElementById("1 " + i), "white", "pawn");
+  }
 }
 
 makechessboard();
