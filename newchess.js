147e57417386e4e3ae2c7c029dc06459a7fcc0e5
kalimbamusic <102989036+kalimbamusic@users.noreply.github.com>
2022-04-27 19:56:54 +0300

    made king moves

---
+++ newchess.js
@@ -0,0 +1,10 @@
+function createboard(){
+const table = document.createElement('table')
+    for (let i = 0; i < 8; i++) {
+        let tr = document.createElement('tr')
+        for (let i = 0; i < 8; i++) {
+            let td = document.createElement('td')
+           
+        }
+    }
+}
