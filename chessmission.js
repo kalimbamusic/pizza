16254369b52f69739b17b8df2bf504854e643d93
kalimbamusic <102989036+kalimbamusic@users.noreply.github.com>
2022-04-27 19:56:31 +0300

    made king moves, and a second click function that doesn't work

--- chessmission.js
+++ chessmission.js
@@ -1,65 +1,27 @@
 
+function onsecondclick(){
+  newmaybecell = document.getElementsByClassName(".maybecell")
+  console.log(newmaybecell)
+}
+
+
 
 function possiblemoves(piecetype, position) {
   document.querySelectorAll(".maybecell").forEach((td) => {
-    td.classList.remove('maybecell')
-    })
+    td.classList.remove('maybecell')})
+ 
+
+  x = parseInt(position[0]);
+  y = parseInt(position[2]);
+  let directions = [[x,y+1],[x,y-1],[x-1,y],[x-1,y-1],[x-1,y+1],[x+1,y],[x+1,y+1],[x+1,y-1]];
+  if (piecetype === '<img src="chess/images/dark/king.png">'||
+  piecetype === '<img src="chess/images/white/king.png">') {
+  for (let i = 0; i < directions.length; i++) {
+  elementById(directions[i][0], directions[i][1]);}}
   
-  positionrow = parseInt(position[0]);
-  positioncolumn = parseInt(position[2]);
-  // const kingmoves = [[0,1],[0,-1] ,[-1,0],[-1,-1],[-1,1],[1,0],[1,1],[1,-1]]
-  const directions = [];
-
-  for (let i = -1; i < 2; i++) {
-      for (let j = -1; j < 2; j++) {
-        directions.push([positionrow + i,positioncolumn+j]) 
-        console.log(directions[0][0])   
-      }
-    }
-    for (let i = 0; i < directions.length; i+=2) {
-      for (let j = 0; j < 8; j++) { 
-      elementById(directions[i][j], directions[i][j])
-      console.log(directions[i][0], directions[i][1])
-      elementById(directions[i][0], directions[i][1])
-    }
-
-    }
-
-
-
-  if (piecetype === '<img src="chess/images/dark/pawn.png">') {
-    elementById(positionrow - 1, positioncolumn);
-  }
-  if (
-    piecetype === '<img src="chess/images/dark/rook.png">' ||
-    piecetype === '<img src="chess/images/white/rook.png">'
-  ) {
-    for (i = 0; i < directions.array; i+=2) {
-      for (let j = 0; j < 8; j++) {
-        elementById(directions[i][j], directions[i][1]);
-      }
-    }
-    for (let j = 0; j < 8; j++) {
-      positionrow = position[0];
-      elementById(positionrow, j);
-    }}
-    
-  if (
-    piecetype === '<img src="chess/images/dark/bishop.png">' ||
-    piecetype === '<img src="chess/images/white/bishop.png">'
-    ) {
-    for (positionrow = 0; positionrow < 8; positionrow++) {
-      elementById(positionrow, positionrow)
-      console.log(positionrow, positionrow) 
-      }
-      for (let positioncolumn = 0; positioncolumn < 8; positioncolumn++) {
-        positionrow = positioncolumn;
-        elementById(positioncolumn, positioncolumn);
-      }
-    
-  }
-  function elementById(positionrow, positioncolumn) {
-    let maybecell = document.getElementById(String(positionrow) + " " + String(positioncolumn));
+    function elementById(x, y) {
+    let maybecell = document.getElementById(String(x) + " " + String(y));
+    console.log(maybecell)
     if(maybecell !== null){
     maybecell.classList.add("maybecell")
     if (maybecell.id === position){
@@ -67,26 +29,25 @@ function possiblemoves(piecetype, position) {
     }
     if(maybecell.innerHTML !== ''){
       maybecell.classList.remove('maybecell')
-      
     }
-    
   }
+  i = document.getElementsByClassName(".maybecell")
+  // i.addEventListener('click',onsecondclick)
 }
 }
 function onclick(td) {
   i = document.querySelector(".chosencell");
   if (i !== null) {
-    i.classList.remove("chosencell");
-  }
+    i.classList.remove("chosencell");}
+  
   td.currentTarget.classList.add("chosencell");
   let piecetype = td.currentTarget.innerHTML;
   let position = td.currentTarget.id;
-  //    console.log(td.currentTarget.innerHTML)
-  //    console.log(td.currentTarget.id)
   possiblemoves(piecetype, position);
 }
+
 function putimageonboard(td, color, type) {
-  const image = document.createElement("img");
+const image = document.createElement("img");
   image.src = "chess/images/" + color + "/" + type + ".png";
   td.appendChild(image);
 }
@@ -143,6 +104,7 @@ makechessboard();
 
 intialboard();
 
+
 // console.log(document.getElementById('0 2'))
 // console.log(document.getElementsByClassName('chosencell').length)
 // console.log(document.getElementById(0).getElementsByClassName('cell'))
@@ -153,9 +115,81 @@ intialboard();
 // })) !== undefined)
 // console.log(document.getElementById('5 7'))
 
-// maybecell = document.getElementById(String(positionrow)+' ' + positioncolumn)
+// maybecell = document.getElementById(String(x)+' ' + y)
 // var maybecellslist = []
 // maybecellslist.push(maybecell)
 // console.log(maybecellslist)
 // maybecellslist[0].classList.add('maybecell')
-// let maybecell = document.getElementById(String(positionrow)+' ' + positioncolumn)
+// let maybecell = document.getElementById(String(x)+' ' + y)
+
+
+
+// function possiblemoves(){
+// x = parseInt(position[0]);
+// y = parseInt(position[2]);
+// let directions = [[x,y+1],[x,y-1],[x-1,y],[x-1,y-1],[x-1,y+1],[x+1,y],[x+1,y+1],[x+1,y-1]];
+// }//(if certin array returned piece, make that array undefined)
+
+
+// 
+// x = parseInt(position[0]);
+// y = parseInt(position[2]);
+// // const kingmoves = [[0,1],[0,-1] ,[-1,0],[-1,-1],[-1,1],[1,0],[1,1],[1,-1]]
+//   const kingmoves = [[x,y+1],[x,y-1],[x-1,y],[x-1,y-1],[x-1,y+1],[x+1,y],[x+1,y+1],[x+1,y-1]]
+
+// const directions = [];
+// for (let i = -1; i < 2; i++) {
+//     for (let j = -1; j < 2; j++) {
+//       directions.push([x + i,y+j]) 
+      
+//     }
+//   }
+//   // for (let i = 0; i < directions.length; i++) {
+//   //   elementById(directions[i][0], directions[i][1])
+//   }
+
+// // console.log(directions)
+// if (piecetype === '<img src="chess/images/dark/pawn.png">') {
+//   elementById(x - 1, y);
+// }
+// if (piecetype === '<img src="chess/images/dark/king.png">'||
+// piecetype === '<img src="chess/images/white/king.png">') {
+//   for (let i = 0; i < directions.length; i++) {
+//   elementById(directions[i][0], directions[i][1]);
+//   }}
+// if (
+//   piecetype === '<img src="chess/images/dark/rook.png">' ||
+//   piecetype === '<img src="chess/images/white/rook.png">'
+// ) {
+//   for (i = 0; i < directions.array; i+=2) {
+//     for (let j = -8; j < 8; j++) {
+//       elementById(directions[i][j], directions[i][1]);
+//     }
+//   }
+//   for (let j = 0; j < 8; j++) {
+//     x = position[0];
+//     elementById(x, j);
+//   }}
+// if (
+//     piecetype === '<img src="chess/images/dark/knight.png">' ||
+//     piecetype === '<img src="chess/images/white/knight.png">'
+//   ){
+
+//   }
+   
+  
+  
+// if (
+//   piecetype === '<img src="chess/images/dark/bishop.png">' ||
+//   piecetype === '<img src="chess/images/white/bishop.png">'
+//   ) {
+//   for (x = 0; x < 8; x++) {
+//     elementById(x, x)
+//     // console.log(x, x) 
+//     }
+//     for (let y = 0; y < 8; y++) {
+//       x = y;
+//       elementById(y, y);
+//     }
+  
+// }
